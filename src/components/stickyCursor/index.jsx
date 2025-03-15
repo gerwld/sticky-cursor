aff9fccb4b13a0b9c160b32d8aeed50ddb179fa3
PJaworski <pjaworski.dev@gmail.com>
2025-03-15 17:34:54 +0100

    Added absDistance based scaling to stickyElement

--- src/components/stickyCursor/index.jsx
+++ src/components/stickyCursor/index.jsx
@@ -1,7 +1,7 @@
 "use client"
 import React, { useEffect, useLayoutEffect, useState } from 'react'
 import style from './style.module.scss';
-import { motion, useMotionValue, useSpring } from 'framer-motion';
+import { motion, useMotionValue, useSpring, transform } from 'framer-motion';
 
 const index = ({stickyElement}) => {
     const [isHovered, setIsHovered] = useState(false);
@@ -17,11 +17,40 @@ const index = ({stickyElement}) => {
         y: useSpring(mouse.y, smoothMouseConfig),
     }
 
+    const scale = {
+        x: useMotionValue(1),
+        y: useMotionValue(1),
+    }
+
     const manageMouseMove = (e) => {
         const {clientX, clientY} = e;
         const {left, top, width, height} = stickyElement.current.getBoundingClientRect();
         const center = {x: left + width / 2, y: top + height / 2}; // center pos of stickyElement
         const distance = {x: clientX - center.x, y: clientY - center.y}
+        
+
+        // найбольшее расстояние не между clientXY, 
+        // а между мышкой и stickyElement, вне зависимости от оси
+        const absDistance = Math.max(Math.abs(distance.x), Math.abs(distance.y));
+        
+        // первое - значение дистанции (то что влияет на scale)
+        // второе - минмакс на выходе (инпут рейндж)
+        // третье - минмакс дистанции (оутпут рейндж)
+        // -- условно есть от 0 до 200 и это нужно переводить в от 1 до 1.4
+        // -- вот оно этим и занимается без считалочки вручную.
+        const newScaleX = transform(
+            absDistance, 
+            [0, width / 2], 
+            [1, 1.4]);
+
+        const newScaleY = transform(
+            absDistance, 
+            [0, width / 2], 
+            [1, 0.8]);
+
+        scale.x.set(newScaleX);
+        scale.y.set(newScaleY);
+
         
         if(isHovered) {
             // mouse.x.set((center.x - CURSOR_SIZE / 2)); // обычное положение без мышки (центр)
@@ -55,7 +84,14 @@ const index = ({stickyElement}) => {
 
   return (
     <motion.div 
-    style={{left: smoothMouse.x, top: smoothMouse.y}}
+    style={{
+        left: smoothMouse.x, 
+        top: smoothMouse.y,
+    
+        scaleX: scale.x,
+        scaleY: scale.y
+    
+    }}
     className={style.cursor}
     animate={{width: CURSOR_SIZE, height: CURSOR_SIZE}}
     >
