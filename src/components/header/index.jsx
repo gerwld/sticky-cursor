da1b90b8e93aafedd7ac85805cda250b7a51830f
PJaworski <pjaworski.dev@gmail.com>
2025-03-15 16:53:32 +0100

    Added "magnetic" onmousemove

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,13 +1,16 @@
 "use client"
 import React, { forwardRef } from 'react'
 import style from './style.module.scss';
+import Magnetic from "@/components/magnetic"
 
-const Header = forwardRef(function index (props, ref) {
+const Header = forwardRef(function index(props, ref) {
   return (
     <div className={style.header}>
-      <div ref={ref} className={style.burger}>
-        <div className={style.bounds}></div>
-      </div>
+      <Magnetic>
+        <div className={style.burger}>
+          <div ref={ref} className={style.bounds}></div>
+        </div>
+      </Magnetic>
     </div>
   )
 });
--- src/components/stickyCursor/index.jsx
+++ src/components/stickyCursor/index.jsx
@@ -20,11 +20,14 @@ const index = ({stickyElement}) => {
     const manageMouseMove = (e) => {
         const {clientX, clientY} = e;
         const {left, top, width, height} = stickyElement.current.getBoundingClientRect();
-        const center = {x: left + width / 2, y: top + height / 2};
+        const center = {x: left + width / 2, y: top + height / 2}; // center pos of stickyElement
+        const distance = {x: clientX - center.x, y: clientY - center.y}
         
         if(isHovered) {
-            mouse.x.set(center.x - CURSOR_SIZE / 2);
-            mouse.y.set(center.y - CURSOR_SIZE / 2);      
+            // mouse.x.set((center.x - CURSOR_SIZE / 2)); // обычное положение без мышки (центр)
+            // mouse.y.set((center.y - CURSOR_SIZE / 2)); // обычное положение без мышки (центр)
+            mouse.x.set((center.x - CURSOR_SIZE / 2 + distance.x / 5)); // с 0.2 от положения мышки
+            mouse.y.set((center.y - CURSOR_SIZE / 2 + distance.y / 5));  // с 0.2 от положения мышки    
         } else {            
             mouse.x.set(clientX - CURSOR_SIZE / 2);
             mouse.y.set(clientY - CURSOR_SIZE / 2);     
